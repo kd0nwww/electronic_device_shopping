90881f39adba1894975055517a3ad52242081531
Kirill Donetskov <kirilldonetskov@gmail.com>
2025-02-25 23:12:52 +0600

    Added checkout method

--- cart.py
+++ cart.py
@@ -5,16 +5,17 @@ class Cart():
 
     def add_device(self, device, amount):
         if device.is_available(amount):
-            self.items[device.name] = amount
+            self.items[device.name] = (device.price, amount)
             self.total_price += device.price * amount
             device.reduce_stock(amount)
+            print("Added to cart successfully.")
         else:
             print(f"Not enough {device.name} available")
 
     def remove_device(self, device, amount):
         if device.name in self.items:
-            if self.items[device.name] >= amount:
-                self.items[device.name] -= amount
+            if self.items[device.name][1] >= amount:
+                self.items[device.name][1] -= amount
                 self.total_price -= device.price * amount
                 device.stock += amount
             else:
@@ -27,14 +28,19 @@ class Cart():
     
     def print_items(self):
         if self.items:
-            for name, amount in self.items.items():
-                print(f"{name}: {amount}")
+            for name, tup in self.items.items():
+                print(f"{name}: {tup[1]}")
         else:
             print("Your cart is empty")
 
     def checkout(self):
         if self.items:
-            for name, amount in self.items.items():
-                print(f"{name}\n")
+            print("-------" * 5)
+            for name, tup in self.items.items():
+                print(f"{name} - {tup[0]}\nx{tup[1]} = {tup[0] * tup[1]}")
+                print("-------" * 5)
+            
+            print(f"Total Price: {self.total_price}")
+            print("-------" * 5)
         else: 
             print("Your cart is empty")
