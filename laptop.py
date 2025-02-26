c31ccc0edd9c627cab9caf8c74abb93c7b08d68e
Kirill Donetskov <kirilldonetskov@gmail.com>
2025-02-26 13:04:43 +0600

    Added units in output

--- laptop.py
+++ laptop.py
@@ -7,7 +7,7 @@ class Laptop(Device):
         self.proccessor_speed = proccessor_speed
 
     def __str__(self):
-        return super().__str__() + f", RAM Size: {self.ram_size}, Proccessor Speed: {self.proccessor_speed}"
+        return super().__str__() + f", RAM Size: {self.ram_size} GB, Proccessor Speed: {self.proccessor_speed} GHz"
     
     def run_program(self):
         print(f"Program is running on {self.name}")
@@ -16,4 +16,4 @@ class Laptop(Device):
         print(f"Keyboard is being used on {self.name}")
 
     def display_info(self):
-        print(f"Device: {self.name}\nPrice: {self.price}\nStock: {self.stock}\nWarranty Period: {self.warranty_period}\nRAM Size: {self.ram_size}\nProccessor Speed: {self.proccessor_speed}")
+        print(f"Device: {self.name}\nPrice: {self.price} USD\nStock: {self.stock}\nWarranty Period: {self.warranty_period} months\nRAM Size: {self.ram_size} GB\nProccessor Speed: {self.proccessor_speed} GHz")
